c4d3e02d7af1f5434ad2d210eb030bf662d63e72
bensbahou <younesbensbahou@gmail.com>
2023-06-06 06:42:03 -0700

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,6 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
  * insert_node - Inserts number into a sorted singly-linked list.
  * @head: Head of the linked list.
