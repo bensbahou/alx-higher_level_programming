bd233c247b8e53fc1714d62f6f20faf8dfa67a82
bensbahou <younesbensbahou@gmail.com>
2023-06-06 06:11:04 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in 'abcdefghijklmnopqrstuvwxyz':
-    if not (chr(i) == 'q' or chr(i) == 'e'):
-        print('{}'.format(chr(i)), end='')
+for letter in 'abcdefghijklmnopqrstuvwxyz':
+    if not (letter == 'q' or letter == 'e'):
+        print('{}'.format(letter), end='')
