b6583d7183d1cbd6bfefdc9d3c6e70aad3b48e86
bensbahou <younesbensbahou@gmail.com>
2023-06-14 06:11:51 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def search_and_replace(my_list, search, replace):
+def search_replace(my_list, search, replace):
     return [replace if elm == search else elm for elm in my_list]
