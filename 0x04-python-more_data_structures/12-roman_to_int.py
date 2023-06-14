cf511b8c5313f07948f724b2f90f71e37488b415
bensbahou <younesbensbahou@gmail.com>
2023-06-14 06:31:45 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str :
+        return 0
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    decs = [roman_dict[x] for x in roman_string]
+    output = 0
+    for i, dec in enumerate(decs):
+        output += dec
+        if i > 0 and dec > decs[i - 1]:
+            output -= 2 * decs[i - 1]
+    return output
