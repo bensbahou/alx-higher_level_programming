322724825b077a7530e6f7fce88886680ae98836
bensbahou <younesbensbahou@gmail.com>
2023-06-14 06:57:46 -0700

    empty c file

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,13 @@
+#include "/usr/include/python3.4/Python.h"
+#include <stdio.h>
+
+void print_python_bytes(PyObject *p)
+{
+	(void)p;
+}
+
+void print_python_list(PyObject *p)
+{
+	/* cast p */
+	(void)p;
+}
