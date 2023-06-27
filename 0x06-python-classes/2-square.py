b0e882c022113ef45faf03e859691bb15eef5c59
bensbahou <younesbensbahou@gmail.com>
2023-06-27 05:02:44 -0700

    fix pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Square class definition"""
+"""Define a class Square."""
 
 
 class Square:
-    """Square class with a private attribute - size."""
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initializes a square
         Args:
             size (int): Size of a side of the square
-        Returns: None
         """
 
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
