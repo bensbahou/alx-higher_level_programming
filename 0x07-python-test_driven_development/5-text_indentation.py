2b81bbdf1c482c1ffde2f821bdd7d5a1b4894e61
bensbahou <younesbensbahou@gmail.com>
2023-07-05 04:10:13 -0700

    my commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,15 +14,11 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     flag = 0
     for a in text:
-        if flag == 0:
-            if a == " ":
-                continue
-            else:
-                flag = 1
+        if flag == 0 and a != " ":
+            flag = 1
         if flag == 1:
-            if a == "?" or a == "." or a == ":":
-                print(a)
-                print()
+            if a in ".?:":
+                print(a, end="\n\n")
                 flag = 0
             else:
                 print(a, end="")
