9e44460ec2c3475e07d01e3d84b8dc2179179f36
bensbahou <younesbensbahou@gmail.com>
2023-07-05 03:44:16 -0700

    my commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The 3-say_my_name module contains a single function called say_my_name.
+The 3-say_my_name module contains a single function called say_my_name
 This function takes a first name and an optional last name, and prints
 "My name is" followed by the first name and optional last name.
 """
@@ -8,7 +8,7 @@ This function takes a first name and an optional last name, and prints
 
 def say_my_name(first_name, last_name=""):
     """Prints "My name is" followed by the first name and optional last name"""
-    
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
