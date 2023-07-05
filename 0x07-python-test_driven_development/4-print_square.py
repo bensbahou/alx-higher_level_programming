ab183ae1f86da8381f39535ed0b741fe676bc73c
bensbahou <younesbensbahou@gmail.com>
2023-07-05 04:02:50 -0700

    my commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,4 +14,4 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size > 0:
-        print(("#" * size + "\n") * size, end="")
+        print("\n".join(["#" * size] * size), end="\n")
