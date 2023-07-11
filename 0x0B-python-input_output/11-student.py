91c0d63a64297fe98d5eb6ff4817cfc5529b737f
bensbahou <younesbensbahou@gmail.com>
2023-07-11 03:14:54 -0700

    0

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,13 +16,9 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves dictionary representation of Student instance"""
 
-        def to_json(self, attrs=None):
-            """
-            Retrieves dictionary representation of Student instance.
-            """
-            if type(attrs) == list and all(type(ele) == str for ele in attrs):
-                return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-            return self.__dict__
+        if type(attrs) == list and all(type(ele) == str for ele in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
         """replaces all attributes of the Student instance"""
