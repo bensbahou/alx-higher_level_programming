3159e130559185285ff3336e6aded6915ab5a23c
bensbahou <younesbensbahou@gmail.com>
2023-07-17 18:51:48 -0700

    fix pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,6 +127,7 @@ class Rectangle(Base):
 
                 if k in switch:
                     setattr(self, switch[k], v)
+
     def to_dictionary(self):
         """Return the  representation of a Rectangle as a dict."""
         return {
