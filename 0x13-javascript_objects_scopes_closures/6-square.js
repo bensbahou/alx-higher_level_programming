81873245cd962f04f186b45274f460c8602f83a7
bensbahou <younesbensbahou@gmail.com>
2023-09-12 11:45:35 +0100

    fix 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,14 @@ const SquareP = require('./5-square');
 
 class Square extends SquareP {
   charPrint(c) {
-    c ||= 'X';
-    console.log((c.repeat(this.width) + '\n').repeat(this.height));
+    c = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
   }
 }
 
