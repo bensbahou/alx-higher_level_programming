d7935639c34291223dab7728a3711e085552bf3c
bensbahou <younesbensbahou@gmail.com>
2023-09-12 11:50:06 +0100

    style 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       [this.width, this.height] = [w, h];
     }
   }
 
-  print() {
+  print () {
     console.log(('X'.repeat(this.width) + '\n').repeat(this.height));
   }
 }
