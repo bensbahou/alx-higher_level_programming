0e56a03b4e3cc304aa8348aaae39563c4766a7e4
bensbahou <younesbensbahou@gmail.com>
2023-10-24 14:41:03 +0100

    fix 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (err, r, body) {
-  if (!err) {
-    const chars = JSON.parse(body).characters;
-    printCharacters(chars, 0);
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
 
-function printCharacters(chars, index) {
-  request(chars[index], function (err, r, body) {
-    if (!err) {
+function printCharacters(characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < chars.length) {
-        printCharacters(chars, index + 1);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
     }
   });
